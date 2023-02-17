b52c01ecb00d0b23abd6ce7e63ec3073972f9707
aravinddemetry31 <96261968+aravinddemetry31@users.noreply.github.com>
2023-02-17 16:49:31 +0530

    Day3 Assignment Submitted

---
+++ Constm.java
@@ -0,0 +1,26 @@
+package operator;
+import java.util.*;
+
+public class Constm {
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		int num;
+		System.out.println("Enter the number: ");
+		num=sc.nextInt();
+         if(num>=0)
+			
+		{
+			System.out.println("Entered number is Positive");
+		}
+		else
+		{
+			System.out.println("Entered number has Negative");
+		}
+	}
+
+}
+
+		
+		
